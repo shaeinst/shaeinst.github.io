6683d1e7d5b1e06667fffb667738e4f2960aba75
Ali Shahid <code@AlixShahid.com>
2022-11-13 18:28:52 +0545

    [huge change] new look

--- frontend/src/components/global/navbar/Navbar.tsx
+++ frontend/src/components/global/navbar/Navbar.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { HomeIcon, MoonIcon, SunIcon } from "../../../assets";
 import "./navbar.scss";
 
 const Navbar = ({ themeModeState }: any) => {
+    const [activeArticle, setActiveArticle] = useState<Boolean>(false);
+    const [homeFillColor, setHomeFillColor] = useState<string>("");
+
+    const location = useLocation();
+
     /* ──────────────────────────────────────────────────── */
     //  change the navigation item's color according to theme-mode
     /* ──────────────────────────────────────────────────── */
@@ -20,91 +25,125 @@ const Navbar = ({ themeModeState }: any) => {
     }, [themeModeState.themeMode]);
     /* ──────────────────────────────────────────────────── */
 
+    useEffect(() => {
+        if (location.pathname === "/articles") {
+            setActiveArticle(true);
+            themeModeState.themeMode === "night"
+                ? setHomeFillColor("#010101")
+                : setHomeFillColor("#ffffff");
+        } else {
+            setActiveArticle(false);
+            setHomeFillColor("#707ef5");
+        }
+    }, [location]);
+
     return (
         <nav className="navbar">
-            {/* ────────────────────────────────────── */}
-            {/* ──────────────── HOME ──────────────── */}
-            <NavLink
-                to="/"
-                style={style}
-                className={({ isActive }) =>
-                    isActive ? "nav__item home active__link" : "nav__item home"
-                }
-                end
-            >
-                <HomeIcon />
-            </NavLink>
+            <div className="navbar__global">
+                <div className="navbar__global-left">
+                    {/* ────────────────────────────────────── */}
+                    {/* ──────────────── HOME ──────────────── */}
+                    <NavLink
+                        to="/"
+                        style={style}
+                        className={"nav__item home"}
+                        end
+                    >
+                        <HomeIcon fill={homeFillColor} stroke={textColor} />
+                    </NavLink>
 
-            {/* ────────────────────────────────────── */}
-            {/* ─────────────── SKILLS ─────────────── */}
-            <NavLink
-                to="/skills"
-                style={style}
-                className={({ isActive }) =>
-                    isActive
-                        ? "nav__item skill active__link"
-                        : "nav__item skill"
-                }
-                end
-            >
-                Skills
-            </NavLink>
+                    {/* ────────────────────────────────────── */}
+                    {/* ─────────────── ARTICLES ────────────── */}
+                    <NavLink
+                        to="/articles"
+                        style={style}
+                        className={({ isActive }) =>
+                            isActive
+                                ? "nav__item articles active__link-global"
+                                : "nav__item articles"
+                        }
+                    >
+                        Articles
+                    </NavLink>
+                </div>
 
-            {/* ────────────────────────────────────── */}
-            {/* ────────────── PROJECTS ────────────── */}
-            <NavLink
-                to="/projects"
-                style={style}
-                className={({ isActive }) =>
-                    isActive
-                        ? "nav__item project active__link"
-                        : "nav__item project"
-                }
-            >
-                Projects
-            </NavLink>
-
-            {/* ────────────────────────────────────── */}
-            {/* ─────────────── EDU_EXP ────────────── */}
-            <NavLink
-                to="/edu_exps"
-                style={style}
-                className={({ isActive }) =>
-                    isActive
-                        ? "nav__item edu_exps active__link"
-                        : "nav__item edu_exps"
-                }
-            >
-                Edu/Exp
-            </NavLink>
-
-            {/* ────────────────────────────────────── */}
-            {/* ─────────────── ARTICLES ────────────── */}
-            <NavLink
-                to="/articles"
-                style={style}
-                className={({ isActive }) =>
-                    isActive
-                        ? "nav__item articles active__link"
-                        : "nav__item articles"
-                }
-            >
-                Articles
-            </NavLink>
-
-            {/* ────────────────────────────────────── */}
-            {/* ───────────── THEME TOGGLE ─────────── */}
-            <div
-                className="theme__toggle"
-                onClick={themeModeState.toggleThemeMode}
-            >
-                {themeModeState.themeMode === "night" ? (
-                    <SunIcon />
-                ) : (
-                    <MoonIcon />
-                )}
+                {/* ────────────────────────────────────── */}
+                {/* ───────────── THEME TOGGLE ─────────── */}
+                <div
+                    className="theme__toggle"
+                    onClick={themeModeState.toggleThemeMode}
+                >
+                    {themeModeState.themeMode === "night" ? (
+                        <SunIcon />
+                    ) : (
+                        <MoonIcon />
+                    )}
+                </div>
+                {/* ────────────────────────────────────── */}
             </div>
-            {/* ────────────────────────────────────── */}
+
+            <div
+                className="navbar__local"
+                style={activeArticle ? { visibility: "hidden" } : {}}
+            >
+                {/* ────────────────────────────────────── */}
+                {/* ──────────────── Home ──────────────── */}
+                <NavLink
+                    to="/"
+                    style={style}
+                    className={({ isActive }) =>
+                        isActive
+                            ? "nav__item home active__link"
+                            : "nav__item home"
+                    }
+                    end
+                >
+                    Home
+                </NavLink>
+
+                {/* ────────────────────────────────────── */}
+                {/* ─────────────── SKILLS ─────────────── */}
+                <NavLink
+                    to="/skills"
+                    style={style}
+                    className={({ isActive }) =>
+                        isActive
+                            ? "nav__item skill active__link"
+                            : "nav__item skill"
+                    }
+                    end
+                >
+                    Skills
+                </NavLink>
+
+                {/* ────────────────────────────────────── */}
+                {/* ────────────── PROJECTS ────────────── */}
+                <NavLink
+                    to="/projects"
+                    style={style}
+                    className={({ isActive }) =>
+                        isActive
+                            ? "nav__item project active__link"
+                            : "nav__item project"
+                    }
+                >
+                    Projects
+                </NavLink>
+
+                {/* ────────────────────────────────────── */}
+                {/* ─────────────── EDU_EXP ────────────── */}
+                <NavLink
+                    to="/edu_exps"
+                    style={style}
+                    className={({ isActive }) =>
+                        isActive
+                            ? "nav__item edu_exps active__link"
+                            : "nav__item edu_exps"
+                    }
+                >
+                    Edu/Exp
+                </NavLink>
+            </div>
         </nav>
     );
 };
