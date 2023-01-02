21f700d8c19a52cd3d931cecb7d776ff12f1e35c
Ali Shahid <code@AlixShahid.com>
2023-01-02 23:06:31 +0545

    remove wave and some UI improvements

--- frontend/src/components/global/sidebar/Sidebar.tsx
+++ frontend/src/components/global/sidebar/Sidebar.tsx
@@ -43,61 +43,63 @@ const Sidebar: FC<Props> = ({ themeMode, screenSize }) => {
                 </div>
             </div>
 
-            <div className="skills">
-                <div className="list">
-                    <ListIcon style={{ fill: `${fillColorList}` }} />
-                    &nbsp; Software Engineer
+            <div className="info">
+                <div className="skills">
+                    <div className="list">
+                        <ListIcon style={{ fill: `${fillColorList}` }} />
+                        &nbsp; Software Engineer
+                    </div>
+                    <div className="list">
+                        <ListIcon style={{ fill: `${fillColorList}` }} />
+                        &nbsp; Researcher
+                    </div>
                 </div>
-                <div className="list">
-                    <ListIcon style={{ fill: `${fillColorList}` }} />
-                    &nbsp; Researcher
+
+                <div className="social__links">
+                    <a
+                        href="https://github.com/shaeinst"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <GithubIcon
+                            style={{ fill: `${fillColor}` }}
+                            className="social github"
+                        />
+                    </a>
+
+                    <a
+                        href="https://linkedin.com/in/shaeinst/"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <LinkedinIcon
+                            style={{ fill: `${fillColor}` }}
+                            className="social linkedin"
+                        />
+                    </a>
+
+                    <a
+                        href="https://twitter.com/shaeinst"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <TwitterIcon
+                            style={{ fill: `${fillColor}` }}
+                            className="social twitter"
+                        />
+                    </a>
+                    <a
+                        href="https://github.com/shaeinst"
+                        // href="mailto:someone@yoursite.com"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <EmailIcon
+                            style={{ fill: `${fillColor}` }}
+                            className="social email"
+                        />
+                    </a>
                 </div>
-            </div>
-
-            <div className="social__links">
-                <a
-                    href="https://github.com/shaeinst"
-                    rel="noreferrer"
-                    target="_blank"
-                >
-                    <GithubIcon
-                        style={{ fill: `${fillColor}` }}
-                        className="social github"
-                    />
-                </a>
-
-                <a
-                    href="https://linkedin.com/in/shaeinst/"
-                    rel="noreferrer"
-                    target="_blank"
-                >
-                    <LinkedinIcon
-                        style={{ fill: `${fillColor}` }}
-                        className="social linkedin"
-                    />
-                </a>
-
-                <a
-                    href="https://twitter.com/shaeinst"
-                    rel="noreferrer"
-                    target="_blank"
-                >
-                    <TwitterIcon
-                        style={{ fill: `${fillColor}` }}
-                        className="social twitter"
-                    />
-                </a>
-                <a
-                    href="https://github.com/shaeinst"
-                    // href="mailto:someone@yoursite.com"
-                    rel="noreferrer"
-                    target="_blank"
-                >
-                    <EmailIcon
-                        style={{ fill: `${fillColor}` }}
-                        className="social email"
-                    />
-                </a>
             </div>
         </div>
     );
--- frontend/src/components/screens/home/Home.tsx
+++ frontend/src/components/screens/home/Home.tsx
@@ -4,15 +4,12 @@ const Home = () => {
     return (
         <div className="home__container">
             <div className="name__detail">
-                <div className="intro">Hi, I'm</div>
+                <div className="intro">I'm</div>
                 <div className="name">Ali Shahid</div>
                 <div className="quote">
                     {/* i can <span>&lt;</span>/code<span>&gt;</span> */}
-                    why are you here?
                 </div>
             </div>
-            {/* NOTE: use App.tsx to set it */}
-            {/* <div className="wave"></div> */}
         </div>
     );
 };
