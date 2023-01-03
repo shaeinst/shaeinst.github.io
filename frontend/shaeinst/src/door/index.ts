f6c1d56738d15dd8444e14d84f4ba5419941bac2
Ali Shahid <shaeinst@gmail.com>
2023-01-03 18:19:06 +0545

    [Refactor] make component for skill card

--- frontend/shaeinst/src/door/index.ts
+++ frontend/shaeinst/src/door/index.ts
@@ -10,6 +10,9 @@ export { default as Projects } from "./screens/home/projects/Projects";
 export { default as Skills } from "./screens/home/skills/Skills";
 export { default as NotFound } from "./screens/notFound/NotFound";
 
+// components
+export { default as CardProject } from "./components/cards/cardProject/CardProject";
+
 // redux
 export { store } from "./utils/redux/store";
 export type { RootState } from "./utils/redux/store";
--- frontend/shaeinst/src/door/screens/home/projects/Projects.tsx
+++ frontend/shaeinst/src/door/screens/home/projects/Projects.tsx
@@ -1,102 +1,35 @@
-// import projectsImage from "../../../assets";
-import { useSelector } from "react-redux";
-import { RootState } from "../../..";
+import { CardProject } from "../../..";
 import "./projects.scss";
 
 const Projects = () => {
-    const { themeMode } = useSelector((state: RootState) => state.theme);
-    const { screenWidth } = useSelector((state: RootState) => state.dimension);
-
-    const CardProject = (
-        direction: string,
-        title: string,
-        description: string,
-        thumbnail: any,
-        url: string
-    ) => {
-        let style_card__thumbnail: any = {
-            backgroundImage: `url(${thumbnail}) `,
-            backgroundColor: themeMode === "day" ? "#ffffff" : "#010101",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-            backgroundSize: "55%",
-        };
-
-        const style_project = {
-            backgroundColor: themeMode === "day" ? "#ffffff" : "#010101",
-        };
-        let style_card__project = {};
-        if (direction === "right") {
-            if (screenWidth < 768) {
-                style_card__project = {
-                    paddingLeft: "4.2rem",
-                    paddingRight: "1rem",
-                };
-
-                style_card__thumbnail = {
-                    ...style_card__thumbnail,
-                    left: "-70px",
-                };
-            } else {
-                style_card__project = {
-                    paddingLeft: "5.5rem",
-                    paddingRight: "1rem",
-                };
-                style_card__thumbnail = {
-                    ...style_card__thumbnail,
-                    left: "-85px",
-                };
-            }
-        }
-
-        return (
-            <div className="card__project" style={style_card__project}>
-                <div className="title"> {title} </div>
-                <div className="description"> {description} </div>
-                <div className="card__thumbnail" style={style_card__thumbnail}>
-                    <div className="view__project" style={style_project}>
-                        <a
-                            href={url}
-                            rel="noreferrer"
-                            target="_blank"
-                            style={{ textDecoration: "none", color: "inherit" }}
-                        >
-                            View Project
-                        </a>
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
     return (
         <div className="container__projects">
             <div className="left">
-                {CardProject(
-                    "left",
-                    "Abstract",
-                    "Abstract is an opensource project foucsed on developing plugins and configs for the IDEs (mainly NEOVIM).",
-                    "https://avatars.githubusercontent.com/u/107783343?s=200&v=4",
-                    "https://github.com/Abstract-IDE"
-                )}
+                <CardProject
+                    direction="left"
+                    title="Abstract"
+                    description="Abstract is an opensource project foucsed on developing plugins and configs for the IDEs (mainly NEOVIM)."
+                    thumbnail="https://avatars.githubusercontent.com/u/107783343?s=200&v=4"
+                    url="https://github.com/Abstract-IDE"
+                />
             </div>
             <div className="right">
-                {CardProject(
-                    "right",
-                    "LiveSportly",
-                    "Place where you can watch live sports for free",
-                    "https://raw.githubusercontent.com/shaeinst/media/main/images/github-repositories/logo.svg",
-                    "https://livesportly.com/"
-                )}
+                <CardProject
+                    direction="right"
+                    title="LiveSportly"
+                    description="Place where you can watch live sports for free"
+                    thumbnail="https://raw.githubusercontent.com/shaeinst/media/main/images/github-repositories/logo.svg"
+                    url="https://livesportly.com/"
+                />
             </div>
             <div className="left">
-                {CardProject(
-                    "left",
-                    "Entrance Question",
-                    "Free and open source project to help students prepare for their entrance exams",
-                    "https://avatars.githubusercontent.com/u/117027421",
-                    "https://github.com/EntranceQuestion"
-                )}
+                <CardProject
+                    direction="left"
+                    title="Entrance Question"
+                    description="Free and open source project to help students prepare for their entrance exams"
+                    thumbnail="https://avatars.githubusercontent.com/u/117027421"
+                    url="https://github.com/EntranceQuestion"
+                />
             </div>
         </div>
     );
