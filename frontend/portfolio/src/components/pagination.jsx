ebbf8b750bd64281e5081abe2d67c7845fc5cac3
4ge101 <theforgettenone2@gmail.com>
2025-07-03 22:20:16 +0545

    making pagination to scroll down the portfolio

--- frontend/portfolio/src/components/pagination.jsx
+++ frontend/portfolio/src/components/pagination.jsx
@@ -1,7 +1,14 @@
+import Arrow_icon from '../assets/icons/arrowdown.svg'
+
 export default function Pagination() {
     return (
         <>
-            <div>pagination</div>
+            <div className="pagination-container">
+                <div className="pagination-dot"></div>
+                <div className="pagination-dot"></div>
+                <div className="pagination-dot"></div>
+                <img src={Arrow_icon} alt="arrow_down_icon" />
+            </div>
         </>
     )
 }
