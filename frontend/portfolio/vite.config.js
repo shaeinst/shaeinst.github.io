9bed57e7ee0a023e0bc03e6a63e46be29f9c028d
4ge101 <theforgettenone2@gmail.com>
2025-07-08 10:44:51 +0545

    iniatl

--- frontend/portfolio/vite.config.js
+++ frontend/portfolio/vite.config.js
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "/portfolio",
+  base: "/shaeinst.github.io",
 })
